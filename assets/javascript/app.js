219cdd29adeea9a4658798c737787b64acf790e4
WD$ <wyntonmdavis@gmail.com>
2019-08-19 08:15:59 -0400

    updating javascript, single question display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,88 @@
+var currentQuestion = 0
 rightAnswers = 0;
 wrongAnswers = 0;
 unanswered = 0;
 
+var container = document.getElementById("#quizContainer");
+var questionEl =document.getElementById(".questions");
+var opt1 = document.getElementById("#opt1");
+var opt2 = document.getElementById("#opt2");
+var opt3 = document.getElementById("#opt3");
+var opt4 = document.getElementById("#opt4");
+
+
+var questions = [{
+    "question": "How many licks does it take to get to the center of a lollipop?",
+    "option1": 364,
+    "option2": 240,
+    "option3": 256,
+    "option4": 325,
+    "answer": 364
+}, {
+    "question": "What is the capital of Texas?",
+    "option1": "Dallas",
+    "option2": "Houston",
+    "option3": "Austin",
+    "option4": "San Antonio",
+    "answer": "Austin"}, 
+                
+    {"question": "What is the tallest standing building on Earth?",
+    "option1": "One World Trade Center",
+    "option2": "Shanghai Tower",
+    "option3": "Burj Khalifa",
+    "option4": "China Zun",
+    "answer": "Burj Khalifa"}];
+
+var totalQuestions = questions.length;
+var nextButton = document.getElementById("#nextBtn")
+
+var results = document.getElementById("#stats");
+
+function loadQuestion(questionIndex) {
+    var q = questions[questionsIndex];
+    questionEl.textContent = (questionsIndex + 1) + ". " + q.questions;
+    opt1.textContent = q.option1;
+    opt2.textContent = q.option2;
+    opt3.textContent = q.option3;
+    opt4.textContent = q.option4;
+}
+
+function loadNextQuestion() {
+var selectedOption = document.querySelector("input[type=radio]:checked");
+    if (!selectedOption){
+        alert("Next question");
+        return;
+    }
+var answer = selectedOption.value;
+
+if(questions[currentQuestion].answer === answer) {
+    rightAnswers++;
+}
+selectedOption.checked = false;
+currentQuestion++;
+
+if(currentQuestion == totalQuestions - 1){
+    nextButton.textContent = "Finish";
+}
+
+if(currentQuestion == totalQuestions){
+    container.style.display ="none";
+    results.style.display =" ";
+    results.textContent ="Your Score" + score;
+    return;
+}
+loadQuestion(currentQuestion);
+}
+
+
+loadQuestion(currentQuestion);
+
+
+
 
 var isCorrect = false;
 
-var questions = [{question: "How many licks does it take to get to the center of a lollipop?",
-                answer: 364}, 
-                {question:" What is the capital of Texas?", 
-                answer: "Austin"}, 
-                {question: "What is the tallest building on Earth?", 
-                answer: "Burj Khalifa"}];
-
 answers = [364, "Austin", "Burj Khalifa"];
-
-incorrectAnswers =[240, 256, 325, "Dallas", "Houston", "San-Antonio"]
-
 
 var submitButton = document.getElementById("#done");
 
@@ -47,81 +114,79 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-$("#stats").attr("color", "red");
-$("#stats").attr("color", "red");
-$("#stats").attr("color", "red");
+// $("#stats").attr("color", "red");
+// $("#stats").attr("color", "red");
+// $("#stats").attr("color", "red");
 
-for (var i = 0; i < answers.length; i++) {
-    if (input === answers[i]){
+// for (var i = 0; i < answers.length; i++) {
+//     if (input === answers[i]){
     
-    }
-    
-}
+//     }
+// }
 
-for (var i = 0; i < questions.length; i++) {
-    answer = prompt(questions[i].question);
+// for (var i = 0; i < questions.length; i++) {
+//     answer = prompt(questions[i].question);
 
-    if (answer !== questions[i].answers) {
-        alert(questions[i].rebuttal);
-        i--;
-    }
-    else {
-        alert(questions[i].affirm);
-    }
-}
+//     if (answer !== questions[i].answers) {
+//         alert(questions[i].rebuttal);
+//         i--;
+//     }
+//     else {
+//         alert(questions[i].affirm);
+//     }
+// }
+// $(function (one) {
+//     $('input[name="How many licks does it take to get to the center of a lollipop?"]').on('click', function() {
+//         if ($(this).val() === 364) {
+//             console.log(rightAnswers++)
+//         } else {
+//         console.log(wrongAnswers++);
+//         }
+//     })
+//     });
 
-$(function (one) {
-    $('input[name="How many licks does it take to get to the center of a lollipop?"]').on('click', function() {
-        if ($(this).val() == "364") {
-            // push to correct answer array
-        } else {
-        // Push to incorrect answer array
-        }
-    })
-    });
+// $(function (two) {
+//         $('input[name="What is the capital of Texas?"]').on('click', function() {
+//             if ($(input).val() === "Austin") {
+//                 console.log(rightAnswers++);
+//             } else {
+//                console.log(wrongAnswers++);
+//             }
+//         })
+//         });
 
-$(function (two) {
-        $('input[name="What is the capital of Texas?"]').on('click', function() {
-            if ($(this).val() === "Austin") {
-                // push to correct answer array
-            } else {
-            // Push to incorrect answer array
-            }
-        })
-        });
-
-$(function (three) {
-            $('input[name="What is the tallest building on Earth?:"]').on('click', function() {
-                if ($(this).val() == "Burj Khalifa") {
-                    // push to correct answer array
-                } else {
-                // Push to incorrect answer array
-                }
-            })
-            });
+// $(function (three) {
+//             $('input[name="What is the tallest building on Earth?"]').on('click', function() {
+//                 if ($(this).val() == "Burj Khalifa") {
+//                     console.log(rightAnswers++);
+//                 } else {
+//                 console.log(wrongAnswers++);
+//                 }
+//             })
+//             });
 
 
 
-var input = $("#done").on("click", function(event){
-        $("#right").text("Correctly Answered: " + rightAnswers);
-        console.log("Correctly Answered : " + rightAnswers);
-        $("#wrong").text("Incorrectly Answered: " + wrongAnswers);
-        console.log("Incorrectly Answered: " + wrongAnswers);
-        $("#unanswered").text("Unanswered: " + unanswered);
-        console.log("Unanswered: " + unanswered);
+// var input = $("#done").on("click", function(event){
+//         $("#right").text("Correctly Answered: " + rightAnswers);
+//         console.log("Correctly Answered : " + rightAnswers);
+//         $("#wrong").text("Incorrectly Answered: " + wrongAnswers);
+//         console.log("Incorrectly Answered: " + wrongAnswers);
+//         $("#unanswered").text("Unanswered: " + unanswered);
+//         console.log("Unanswered: " + unanswered);
 
 
-});
+// });
 
-function begin() { window.onload = function () {
-    var fiveMinutes = 10,
-        display = document.querySelector('.time-rem');
-        startTimer(fiveMinutes, display);
-};
-}
-$(".start").on("click", function(){
-    clearInterval(startTimer());
-    var fiveMinutes = 10,
-        display = document.querySelector('.time-rem');
-        startTimer(fiveMinutes, display);
-});
+// function begin() { window.onload = function () {
+//     var fiveMinutes = 10,
+//         display = document.querySelector('.time-rem');
+//         startTimer(fiveMinutes, display);
+// };
+// }
+// $(".start").on("click", function(){
+//     clearInterval(startTimer());
+//     var fiveMinutes = 10,
+//         display = document.querySelector('.time-rem');
+//         startTimer(fiveMinutes, display);
+// });
